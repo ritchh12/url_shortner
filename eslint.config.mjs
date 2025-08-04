81ecef02617659f30293fb014a4774b3f2526728
ritchh12 <riteshsingh3013@gmail.com>
2025-08-04 11:59:54 +0530

    Fix ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,14 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      // Disable problematic parser serialization
+      "@next/next/no-build-warnings": "off"
+    }
+  }
+];
 
 export default eslintConfig;
